1ded31c77132c83b8ae29eb7da4247a29f036b84
Neo-X <glen@fracturedplane.com>
2020-01-29 13:55:30 -0800

    Working on PPO

--- functional_scripts/local_test_ppo.py
+++ functional_scripts/local_test_ppo.py
@@ -1,5 +1,8 @@
 import sys
 
+## TODO: Batch size vs amount of data collected
+## TODO: Number of PPO update steps
+
 from rllab.misc.comet_logger import CometLogger
 # comet_logger = CometLogger(api_key="KWwx7zh6I2uw6oQMkpEo3smu0",
 #                             project_name="ml4l3", workspace="glenb")
